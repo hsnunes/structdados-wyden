f907c4d627ef952150dc230bdade0f2c1aff9a32
Hilder Nunes <hildersn@gmail.com>
2022-05-17 11:12:15 -0300

    Estrutura inicial dos programas C++ na disciplina

---
+++ Funcao.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <locale.h> // setLocale
+#include <stdlib.h> // System
+#include <string>   // Textos em geral
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+
+
+    system("pause");
+    return 0;
+}
