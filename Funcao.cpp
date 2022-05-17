09be15c7ffe4a915635d8e940910d6066a3db6e1
Hilder Nunes <hildersn@gmail.com>
2022-05-17 11:45:27 -0300

    Criando funções e utilizando no programa

--- Funcao.cpp
+++ Funcao.cpp
@@ -5,12 +5,48 @@
 
 using namespace std;
 
+// Criando uma função, para imprimir na tela ALo Mundo
+// void imprimirMsg() {
+//     cout <<"Alô Mundo!" << endl;
+// }
+
+// Declarando a função para boas práticas, quando ela está abaixc do main
+// Função que não retorna nada
+void imprimirMsg();
+// Fn que retorna um float
+float calculaImposto();
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
+    float res;
 
+    imprimirMsg();
+
+    // Recebendo o resultado da Fn, na variavel res
+    res = calculaImposto();
+
+    cout <<"O Desconto do Imposto foi: " << res << endl;
 
     system("pause");
     return 0;
+}
+
+// Criando uma função, para imprimir na tela ALo Mundo
+void imprimirMsg() {
+    cout <<"Alô Mundo!" << endl;
+}
+
+float calculaImposto()
+{
+    float salario;
+    float valImposto;
+
+    salario = 2824;
+
+    // Calculo de 10%
+    valImposto = salario * 0.1;
+
+    return valImposto;
 }
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # structdados-wyden
 Estudos de Estruturas de Dados com C++
 Disciplina 22-1 (2)
+
+
+Funções
+
+Bibliotecas
+
+boas praticas de posicionamento das funcoes
+
+Camel Case;
